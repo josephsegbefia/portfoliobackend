fc91fffcaeb696f9687aca97243b56811f5f4d0d
Joseph Segbefia <elorma.segbefia@gmail.com>
2023-08-24 12:14:02 +0200

    Reload 4

--- middleware/jwt.middleware.js
+++ middleware/jwt.middleware.js
@@ -4,8 +4,7 @@ const { expressjwt: jwt } = require("express-jwt");
 
 // Instantiate the JWT token validation middleware
 const isAuthenticated = jwt({
-  secret: "kaulsdorf-nord",
-  // process.env.TOKEN_SECRET,
+  secret: process.env.TOKEN_SECRET,
   algorithms: ["HS256"],
   requestProperty: "payload",
   getToken: getTokenFromHeaders
